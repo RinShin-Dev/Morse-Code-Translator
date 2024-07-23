c00d653eb830f47721e248e067b4d83d88f9cc6d
6MA-606 <zyxma.6666@hotmail.com>
2024-07-23 15:30:01 +0700

    Add: Prototype UI

---
+++ src/components/IconSVG.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import { ref, onMounted } from 'vue'
+
+const props = defineProps({
+  iconName: {
+    type: String,
+    required: true
+  },
+  color: String,
+  scale: {
+    type: [Number, String],
+    default: 1
+  },
+  size: {
+    type: [Number, String],
+    default: '1rem'
+  }
+})
+
+const icon = ref('')
+
+onMounted(async () => {
+  icon.value = (await import(`@/assets/icons/${props.iconName}.svg?raw`)).default
+})
+
+</script>
+
+<template>
+  <div
+    :style="{ width: size, height: size }"
+    class="grid place-items-center"
+  >
+    <div
+      v-html="icon"
+      :style="{
+        color,
+        transform: `scale(${scale})`
+      }"
+    ></div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup>
+import { ref, computed } from 'vue'
+
+const mode = ref('encode')
+const input = ref('')
+const output = computed(() => {
+  //TODO: Implement the morse code translation
+  if (input.value === '') return mode.value + 'd output'
+  if (mode.value === 'encode') {
+    return 'encoded output from ' + input.value
+  } else {
+    return 'decoded output from ' + input.value
+  }
+})
+
+const handleChangeMode = () => {
+  mode.value = mode.value === 'encode' ? 'decode' : 'encode'
+}
+
 </script>
 
 <template>
   <main>
-    <h1 class="text-xl">Home</h1>
-    <p>Welcome to the Home page</p>
+    <h1 class="text-xl">Morserera</h1>
+    <p>Morse code translator</p>
+    <div class="flex items-center gap-3">
+      <textarea v-model="input"
+        :placeholder="mode === 'encode' ? 'Input text to encode' : 'Input morse code to decode (use / for word separation)'"
+        class="textarea textarea-bordered resize-none w-[25rem] h-[10rem]"></textarea>
+      <button class="btn" @click="handleChangeMode">
+        <div class="flex flex-col gap-1">
+          <div>Switch Mode</div>
+          <div class="text-xs">Now is <span class="text-red-400">{{ mode }}</span> mode</div>
+        </div>
+      </button>
+      <div
+        class="textarea textarea-bordered bg-base-200 text-base-content text-opacity-75 w-[25rem] h-[10rem] break-words">
+        {{ output }}
+      </div>
+    </div>
   </main>
 </template>
