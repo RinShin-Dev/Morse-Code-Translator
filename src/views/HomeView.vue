f4bc6a37051db01aa09041e8250c3464108dff3d
6MA-606 <zyxma.6666@hotmail.com>
2024-07-28 23:52:08 +0700

    Update: Cheesy UI 1.0

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,6 @@ import { encode, decode } from '../utils/morse.js'
 const mode = ref('encode')
 const input = ref('')
 const output = computed(() => {
-  if (input.value === '') return mode.value + 'd output'
   if (mode.value === 'encode') {
     return encode(input.value)
   } else {
@@ -14,29 +13,44 @@ const output = computed(() => {
 })
 
 const handleChangeMode = () => {
+  if (output.value !== '') {
+    input.value = output.value
+  }
   mode.value = mode.value === 'encode' ? 'decode' : 'encode'
 }
 
 </script>
 
 <template>
-  <main>
-    <h1 class="text-xl">Morserera</h1>
-    <p>Morse code translator</p>
-    <div class="flex items-center gap-3">
-      <textarea v-model="input"
-        :placeholder="mode === 'encode' ? 'Input text to encode' : 'Input morse code to decode (use / for word separation)'"
-        class="textarea textarea-bordered resize-none w-[25rem] h-[10rem]"></textarea>
-      <button class="btn" @click="handleChangeMode">
-        <div class="flex flex-col gap-1">
-          <div>Switch Mode</div>
-          <div class="text-xs">Now is <span class="text-red-400">{{ mode }}</span> mode</div>
+  <main class="mt-4">
+    <section class="flex flex-col md:flex-row items-center md:justify-center gap-3">
+      <div class="w-[90%] md:w-[40%]">
+        <div class="text-sm">Input</div>
+        <textarea v-model="input"
+          :placeholder="mode === 'encode' ? 'Text to encode' : 'Enter morse code to decode. (You can use . and - for morse code, space for character separation, and / for word separation.)'"
+          class="textarea textarea-bordered resize-none w-full h-[10rem] border-2 border-b-primary placeholder:text-sm">
+        </textarea>
+      </div>
+      <button
+        class="relative bg-secondary h-10 w-36 rounded-lg font-morserera overflow-hidden shadow-[3px_3px_0px] shadow-accent transition active:shadow-[0px_0px_0px] active:translate-x-[3px] active:translate-y-[3px]"
+        @click="handleChangeMode">
+        <div class="absolute flex inset-0 justify-evenly items-center text-accent">
+          <div>Text</div>
+          <div class="text-xs">to</div>
+          <div>Text</div>
+        </div>
+        <div :class="mode === 'decode' ? 'translate-x-0' : 'translate-x-[125%]'"
+          class="relative w-[45%] h-full bg-primary rounded-lg grid place-items-center transition text-accent">
+          <div>Morse</div>
         </div>
       </button>
-      <div
-        class="textarea textarea-bordered bg-base-200 text-base-content text-opacity-75 w-[25rem] h-[10rem] break-words">
-        {{ output }}
+      <div class="w-[90%] md:w-[40%]">
+        <div class="text-sm">Output</div>
+        <div
+          class="textarea textarea-bordered bg-base-200 text-base-content text-opacity-75 w-full h-[10rem] break-words border-2 border-b-primary">
+          {{ output }}
+        </div>
       </div>
-    </div>
+    </section>
   </main>
 </template>
